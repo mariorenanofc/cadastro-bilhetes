58180d492169f88dd075995eff588d51cfdfe0f1
mariorenanofc <mariorenan25@gmail.com>
2023-01-02 12:35:13 -0300

    renderização de lista

--- rifa-digital/src/components/Info.vue
+++ rifa-digital/src/components/Info.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <p>Seguintes tecnólogias para Backend de Site:</p>
+        <ul >
+            <li v-for="(techlings, index) in backend_tecnologia" v-bind:key="index" >{{ techlings }}</li>
+        </ul>
+        <p>Seguintes tecnólogia para o FrontEnd do Site?</p>
+        <ul>
+            <li v-for="techlings in frontend_tecnologia" :key="techlings.id">{{ techlings.language }}</li>
+        </ul>
         
         <p>Para Acessar meu portifólio basta <a target="_blank" v-bind:href="meu_link">clicar aqui</a></p>
         <Picture />
@@ -12,8 +20,16 @@ import Picture from './Picture.vue'
   components: { Picture },
         data() {
             return {
-            meu_link: 'https://mariorenanofc.github.io/Portifo-lio/'
+            meu_link: 'https://mariorenanofc.github.io/Portifo-lio/',
+            backend_tecnologia: ["Javascript","MySQL","Python"],
+            frontend_tecnologia:[
+                {id: 1, language: 'ScriptJS'},
+                {id: 2, language: 'VueJS'},
+                {id: 3, language: 'TypeScript'},
+            ]
         }
-        }
+        },
+
     }
+
 </script>
