350003cf0a2311a7712c24c8b71c27f1f06606ee
mariorenanofc <mariorenan25@gmail.com>
2023-01-02 16:12:18 -0300

    Eventos @Submit e @click

--- rifa-digital/src/components/Form.vue
+++ rifa-digital/src/components/Form.vue
@@ -7,6 +7,30 @@ import Submit from './form/Submit.vue';
         components: {
             InputText,
             Submit
+        },
+        data() {
+            return {
+                name: "",
+                number: ""
+            }
+        },
+        methods: {
+            enviarformulario(e) {
+
+                e.preventDefault();
+
+                const name = this.name;
+                const number = this.number;
+
+
+                console.log("Seu nome é: " + name);
+                console.log("Seu N/S é: " + number);
+                console.log("Formulario Enviado");
+
+                //Ajax
+
+                // Inserir no banco de Dados
+            }
         }
     }
 </script>
@@ -15,14 +39,20 @@ import Submit from './form/Submit.vue';
 <template>
     <div>
         <h2>Preencher dados</h2>
-        <form action="">
+        <form action="" @submit="enviarformulario($event)">
        <div>
         <label for="name"></label>
-           <InputText placeholder="Nome completo" />
+        <input type="text" v-model="name" placeholder="Digite seu nome">
+           <!--
+            <InputText placeholder="Nome completo" />
+            -->
        </div>
        <div>
+        <input type="number" v-model="number" placeholder="Digite N/S" >
+        <!--
             <label for="idBilhete"></label>
            <InputText type="number" placeholder="Número de Série" />
+        -->
        </div>
        <div>
            <Submit />
