3b7676c0821c2c7aa4a631d34bdfa755d1784666
mariorenanofc <mariorenan25@gmail.com>
2023-01-05 16:41:11 -0300

    "feat: Usando recurso de emit para ouvir eventos"

---
+++ rifa-digital/src/components/MudarImagem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')">Troca imagem da Logo</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MudarImagem',
+        emits: ['mudarImagem'],
+
+        methods: {
+            
+        }
+        
+    }
+</script>
--- rifa-digital/src/components/Picture.vue
+++ rifa-digital/src/components/Picture.vue
@@ -1,14 +1,27 @@
 <template>
-    <img :src="avatar" :alt="descricao" />
+    <div>
+        <img :src="avatar" :alt="descricao" />
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue';
+
     export default {
         name: 'Picture',
+        components: {
+        MudarImagem 
+        },
         data() {
             return {
                 avatar: "/img/logo.png",
                 descricao: "Polo da Sorte"
+            }
+        },
+        methods: {
+            trocarImagem(){
+                this.avatar = "/img/logo-sem-fundo.png"
             }
         }
     }
